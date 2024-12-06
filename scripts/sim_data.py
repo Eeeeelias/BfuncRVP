5fe6520c1a9824ba0b39102fd1d1ef7cfdbed056
RLuknarova <r.luknarova@gmail.com>
2024-12-06 14:10:46 +0100

    Dataset class defined

--- scripts/sim_data.py
+++ scripts/sim_data.py
@@ -1,4 +1,13 @@
+from torch.utils.data import Dataset
 
-
-
+# defining a new simulated Dataset
+class Sim_Dataset(Dataset):
+    def __init__(self, n_patients = 160000, n_genes = 20000, n_dim = 256):
+        pass
+    def simulate_phenotype(self, n_patients):
+        pass
+    def simulate_gt_betas(self, n_genes):
+        pass
+    def create_func_embeddings(self, n_genes, n_dim):
+        pass
 
