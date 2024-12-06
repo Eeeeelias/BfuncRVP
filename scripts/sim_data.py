0d05dd44bf38b11495cd7e21b351c34fc983c394
RLuknarova <r.luknarova@gmail.com>
2024-12-06 18:21:50 +0100

    simulating functional embedding

--- scripts/sim_data.py
+++ scripts/sim_data.py
@@ -4,18 +4,22 @@ import numpy as np
 # defining a new simulated Dataset
 class Sim_Dataset(Dataset):
     '''
-    n_patients = number of patients for which the phenotype is simulated, default is 2 000 
+    n_patients = number of patients for which the phenotype is simulated, default is 2 000
     n_genes = number of tested genes for which the beta and functional embedding should be simulated, default 20 000 as usually is
     n_dim = dimension of the functional embedding, defualt 256 to which the func. embedding was reduced to in FuncRVP preprint
+    repeat = how many genes should have the same positive and negative beta and share embedding dimension (2*repeat)
     '''
-    def __init__(self, n_patients = 2000, n_genes = 20000, n_dim = 256):
+    def __init__(self, n_patients = 2000, n_genes = 20000, n_dim = 256, repeat = 5):
+        self.repeat = repeat
         self.betas = self.simulate_gt_betas(n_genes)
         self.g_matrix = self.simulate_g_matrix(n_patients, n_genes)
         self.phenos = self.compute_phenotype()
+        self.f_embedding = self.create_func_embeddings(n_genes, n_dim)
     def simulate_gt_betas(self, n_genes):
         abs_beta = 5
-        repeat = 5
-        betas = np.array([abs_beta]*repeat + [-abs_beta]*repeat + list(np.random.normal(0, 1, n_genes - 2*repeat)))
+        betas = np.array([abs_beta]*self.repeat +
+                         [-abs_beta]*self.repeat +
+                         list(np.random.normal(0, 1, n_genes - 2*self.repeat)))
         return betas
 
     def simulate_g_matrix(self, n_patients, n_genes):
@@ -32,6 +36,12 @@ class Sim_Dataset(Dataset):
         return phenotype_binary
 
     def create_func_embeddings(self, n_genes, n_dim):
-        pass
+        n_shared_dim = 30 # how many dimensions should be shared between the first genes
+        shared = np.random.normal(0, 1, n_shared_dim)
+        func_emb = np.random.normal(0, 1, size=(n_genes, n_dim))
+        for i in range(self.repeat*2):
+            func_emb[i, :n_shared_dim] = shared
+            
+        return func_emb
 
 n_data = Sim_Dataset()
